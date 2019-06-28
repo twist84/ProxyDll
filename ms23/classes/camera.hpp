a8907d9f742988738139b0f1b8e59e2cf72173b9
theTwist84 <lee.foulger.94@gmail.com>
2019-06-28 22:20:14 +0100

    Added `director_globals_set_director_mode` hook Added the beginning of a new camera switching implementation (to replace for ElDewrito)

--- ms23/classes/camera.hpp
+++ ms23/classes/camera.hpp
@@ -10,18 +10,46 @@ char __cdecl sub_614CB0_hook(uint8_t *a1)
 	return result;
 }
 
+void observer_camera_mode_update(uint8_t camera_mode)
+{
+	// default is 3:first_person
+	*(uint8_t *)0x726449 = camera_mode;
+}
+
+uint8_t *__cdecl director_globals_set_director_mode_hook(int local_player_index, int director_mode)
+{
+	auto dir_mode = ConfigManager.GetInt("Camera", "DirectorMode");
+	if (dir_mode != -1)
+		director_mode = dir_mode;
+
+	auto cam_mode = ConfigManager.GetInt("Camera", "CameraMode");
+	if (dir_mode == 3 && cam_mode != -1) observer_camera_mode_update(cam_mode);
+
+	auto result = ((uint8_t * (__cdecl *)(int, int))(0x592480))(local_player_index, director_mode);
+
+	printf_s("player_index:%d, director_mode:%d\n", local_player_index, director_mode);
+	return result;
+}
+
 inline void AddCameraHooks(const char *name)
 {
 	if (ConfigManager.GetBool("Hooks", name))
 	{
 		HookManager.AddHook({ 0x214D68 }, &sub_614CB0_hook, "camera_definition_validate", HookFlags::IsCall);
+
+		HookManager.AddHook({ 0x19228B, 0x192399 }, &director_globals_set_director_mode_hook, "director_globals_set_director_mode", HookFlags::IsCall);
 	}
+}
+
+void observer_camera_mode_patch()
+{
+	observer_camera_mode_update(3);
 }
 
 inline void AddCameraPatches(const char *name)
 {
 	if (ConfigManager.GetBool("Patches", name))
 	{
-
+		PatchManager.AddPatch(&observer_camera_mode_patch, "observer_camera_mode");
 	}
 }
